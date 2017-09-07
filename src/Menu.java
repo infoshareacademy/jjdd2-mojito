0700fd08406415b9ef57254f4aa50c7755c4527a
Tomek Zmitrowicz <tzmitrowicz@wp.pl>
2017-09-07 16:15:12 +0200

    test

--- src/Menu.java
+++ src/Menu.java
@@ -13,40 +13,46 @@ public class Menu {
             System.out.println("5.Statystyki ilosci stacji rowerowych w danym panstwie.");
             System.out.println("6.Statystyki ilosci stacji rowerowych w danym miescie");
             int input = scanner.nextInt();
-            while(true){
-                try{
+            while (true) {
+                try {
                     input = scanner.nextInt();
                     break;
-                }
-                catch (InputMismatchException e) {
+                } catch (InputMismatchException e) {
                     System.out.print("Invalid input. Please reenter: ");
 
-            }
-                switch(input){
-                case 1:input=1;
-                    System.out.println("jeden");
-                    // wyszukiwanieNajStacji;
-                    break;
-                case 2:input=2;
-                    // Wyszukanie stacji w danym kraju.
-                    break;
-                case 3:input=3;
-                    //Wyszukanie stacji w danym miescie.
-                    break;
-                case 4:input=4;
-                    System.out.println("cztery");
-                    // Wyszukanie stacji rowerowerej na danym obszarze
-                    break;
-                case 5:input=5;
-                    //Statystyki ilosci stacji rowerowych w danym panstwie.
-                    break;
-                case 6:input=6;
-                    // Statystyki ilosci stacji rowerowych w danym miescie
-                    break;
-                default:
-                    //prosze wybrac komende
-                    //koniec
+                }
+                switch (input) {
+                    case 1:
+                        input = 1;
+                        System.out.println("jeden");
+                        // wyszukiwanieNajStacji;
+                        break;
+                    case 2:
+                        input = 2;
+                        // Wyszukanie stacji w danym kraju.
+                        break;
+                    case 3:
+                        input = 3;
+                        //Wyszukanie stacji w danym miescie.
+                        break;
+                    case 4:
+                        input = 4;
+                        System.out.println("cztery");
+                        // Wyszukanie stacji rowerowerej na danym obszarze
+                        break;
+                    case 5:
+                        input = 5;
+                        //Statystyki ilosci stacji rowerowych w danym panstwie.
+                        break;
+                    case 6:
+                        input = 6;
+                        // Statystyki ilosci stacji rowerowych w danym miescie
+                        break;
+                    default:
+                        //prosze wybrac komende
+                        //koniec
 
+                }
             }
         }
 }
