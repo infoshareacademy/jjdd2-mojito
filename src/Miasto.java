98499366faa4575fe047605cdd63d594cba9c34d
Tomek Zmitrowicz <tzmitrowicz@wp.pl>
2017-09-07 16:36:12 +0200

    utworzene klasy Panstwo i Miasto

--- src/Miasto.java
+++ src/Miasto.java
@@ -1,2 +1,29 @@
 public class Miasto {
+    private String nazwa;
+    private Double szerokosc;
+    private Double dlugosc;
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public Double getSzerokosc() {
+        return szerokosc;
+    }
+
+    public void setSzerokosc(Double szerokosc) {
+        this.szerokosc = szerokosc;
+    }
+
+    public Double getDlugosc() {
+        return dlugosc;
+    }
+
+    public void setDlugosc(Double dlugosc) {
+        this.dlugosc = dlugosc;
+    }
 }
--- src/Panstwo.java
+++ src/Panstwo.java
@@ -1,6 +1,23 @@
-public class Panstwo {
-    @Override
-    public String Panstwo {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Panstwo {
+    private String nazwa ;
+    private List<Miasto> listaMiast = new ArrayList<>();
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public List<Miasto> getListaMiast() {
+        return listaMiast;
+    }
+
+    public void setListaMiast(List<Miasto> listaMiast) {
+        this.listaMiast = listaMiast;
     }
 }
