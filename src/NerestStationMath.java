da46420a3963d6bf49bcca8f941d3c54c31d04cf
mkoniuszy <m.koniuszy@vp.pl>
2017-09-12 16:59:53 +0200

    1.2 pobranie danych , porownianie , wynik , zmiana typu String na double, dodanie przeliczania km , brak bazy stacji !pomysl na jd2m-13

---
+++ src/NerestStationMath.java
@@ -0,0 +1,10 @@
+public class NerestStationMath {
+
+    protected static String Dupa(){
+        return null;
+    }
+}
+//dupa[0] + dupa[1] = Double x ;
+//dup1[0] + dup2[1] = Double y;
+odleglosc = Double 5.0;
+if (x+odleglosc != y );
