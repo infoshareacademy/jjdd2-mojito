0be02e590041d5e972292cb22a91cc54344aa3ba
mkoniuszy <31629817+mkoniuszy@users.noreply.github.com>
2017-09-15 19:40:03 +0200

    Jd2 m 25 (#3)
    * menuv1
    
    * menu wersja 1.1
    
    * ver.1.4
    
    * 111
    
    * ver 1.5
    
    * ver 1.5 zmiana try ,catch i zamkniecie programu
    
    * ver 1.5 zmiana try ,catch i zamkniecie programu,poprawienie petli while
    
    * ver.1.5
    
    * ver.1.6 drobne poprawki
    
    * Gotowe menu .
    
    * Menu
    
    * Menu z poprawkami
    
    * Jd2m-25 poprawki
    
    * Jd2m-25 poprawki
    
    * porawianie metod na nie static itp .

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,15 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
 public class Menu {
+
+
+    public static void main(String[] args) {
+
+        MenuText menuText= new MenuText();
+        menuText.text();
+        UserChooseFromMenu choose = new UserChooseFromMenu();
+        choose.userChooseFromMenu();
+    }
 }
