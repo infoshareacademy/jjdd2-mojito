cd7b4f6fdd4ce60742b303c1491b9830cea9cbfb
mkoniuszy <m.koniuszy@vp.pl>
2017-09-16 19:19:33 +0200

    powoli klejenie wszystkiego do kupy

--- src/Country.java
+++ src/Country.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
+
 public class Country {
+    private  String name;
 
+    private Double latitude;
 
+    private Double longitude;
+
+    public City(Double latitude, Double longitude, String name,String country) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.name = country;
+    }
 }
