0947c11af9a78a28240b941c43d0c3de05470b77
mkoniuszy <m.koniuszy@vp.pl>
2017-09-18 10:21:26 +0200

    hot fix

--- src/infoshare/kurs/mohito/rowery/UserChooseFromMenu.java
+++ src/infoshare/kurs/mohito/rowery/UserChooseFromMenu.java
@@ -1,38 +1,25 @@
 package infoshare.kurs.mohito.rowery;
-
-import org.xml.sax.SAXException;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
+import java.util.List;
 
 public class UserChooseFromMenu {
-    protected  void userChooseFromMenu(){
+    protected  void userChooseFromMenu(List<City> cityList){
         UserInputReader scanner = new UserInputReader();
         AfterTask aftertask=new AfterTask();
-        BikeParsing bikeParsing = new BikeParsing("nextbike-live.xml");
-        try {
-            bikeParsing.parseData();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
         String input = "0";
         while (true) {
             try {
                 input =(scanner.readlineString());
             } catch (NumberFormatException e) {
-                System.out.println("Wybierz jedna z opcji infoshare.kurs.mohito.rowery.Menu wpisujac liczby od 1-7 ");
+                System.out.println("Wybierz jedna z opcji Menu wpisujac liczby od 1-7 ");
                 continue;
             }
             switch (input) {
                 case "1":
                     GeoLocation geoLocation = new GeoLocation();
                     geoLocation.geoLocation();
-                    NearestPlace nearestPlace = new NearestPlace();
-                    nearestPlace.findNearestPlace(geoLocation,bikeParsing);
+                    NearestPlace nearestPlace = new NearestPlace(cityList);
+                    nearestPlace.findNearestPlace(geoLocation);
                     aftertask.aftertask();
                     break;
                 case "2":
--- src/infoshare/kurs/mohito/rowery/UserChooseRadius.java
+++ src/infoshare/kurs/mohito/rowery/UserChooseRadius.java
@@ -19,49 +19,49 @@ public class UserChooseRadius {
         System.out.println("1.szukaj w odleg1osci 5 km");
         System.out.println("2.szukaj w odleg1osci 10 km");
         System.out.println("3.szukaj w odleg1osci 15 km");
-        System.out.println("4.Wróc do infoshare.kurs.mohito.rowery.Menu .");
+        System.out.println("4.Wróc do Menu .");
 
         String input = "0";
         while (true) {
             try {
                 input = scanner.readlineString();
             } catch (NumberFormatException e) {
-                System.out.println("Wybierz jedna z opcji infoshare.kurs.mohito.rowery.Menu wpisujac liczby od 1-4 ");
+                System.out.println("Wybierz jedna z opcji Menu wpisujac liczby od 1-4 ");
                 continue;
             }catch (NullPointerException e){
-                System.out.println("Wybierz jedna z opcji infoshare.kurs.mohito.rowery.Menu wpisujac liczby od 1-4 ");
+                System.out.println("Wybierz jedna z opcji Menu wpisujac liczby od 1-4 ");
             }
             switch (input) {
                 case "1":
                     GeoLocation geoLocation5 = new GeoLocation();
                     geoLocation5.geoLocation();
-                    NearestPlace nearestPlace = new NearestPlace();
-                    nearestPlace.findPlace(geoLocation5,bikeParsing,5.0);
+                    NearestPlace nearestPlace = new NearestPlace(bikeParsing.getCityList());
+                    nearestPlace.findPlace(geoLocation5,5.0);
                     afterTask.aftertask();
                     break;
                 case "2":
                     //wyszukanie staci w odleglosci 10km.
                     GeoLocation geoLocation10 = new GeoLocation();
                     geoLocation10.geoLocation();
-                    NearestPlace nearestPlace10 = new NearestPlace();
-                    nearestPlace10.findPlace(geoLocation10,bikeParsing,10.0);
+                    NearestPlace nearestPlace10 = new NearestPlace(bikeParsing.getCityList());
+                    nearestPlace10.findPlace(geoLocation10,10.0);
                     afterTask.aftertask();
                     break;
                 case "3":
                     //wyszukanie staci w odleglosci 15km.
                     GeoLocation geoLocation15 = new GeoLocation();
                     geoLocation15.geoLocation();
-                    NearestPlace nearestPlace15 = new NearestPlace();
-                    nearestPlace15.findPlace(geoLocation15,bikeParsing,15.0);
+                    NearestPlace nearestPlace15 = new NearestPlace(bikeParsing.getCityList());
+                    nearestPlace15.findPlace(geoLocation15,15.0);
                     afterTask.aftertask();
                     break;
                 case "4":
                     UserChooseFromMenu returnMenu = new UserChooseFromMenu();
-                    returnMenu.userChooseFromMenu();
+                    returnMenu.userChooseFromMenu(bikeParsing.getCityList());
                     afterTask.chooseAfterTask();
                     break;
                 default:
-                    System.out.println("Wybierz jedna z opcji infoshare.kurs.mohito.rowery.Menu wpisujac liczby od 1-4 ");
+                    System.out.println("Wybierz jedna z opcji Menu wpisujac liczby od 1-4 ");
 
             }
         }
