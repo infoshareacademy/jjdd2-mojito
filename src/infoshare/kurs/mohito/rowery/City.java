8201dc1be56fbda664e382df97e9d8ec9ce654a0
mkoniuszy <31629817+mkoniuszy@users.noreply.github.com>
2017-09-18 11:36:06 +0200

    All skoczone !  (#5)
    * Initial commit
    
    * Create .gitignore
    
    * menuv1
    
    * menu wersja 1.1
    
    * ver.1.4
    
    * 111
    
    * ver 1.5
    
    * dodanie klas Państwo i miasto
    
    * test
    
    * ver 1.5 zmiana try ,catch i zamkniecie programu
    
    * utworzene klasy Panstwo i Miasto
    
    * ver 1.5 zmiana try ,catch i zamkniecie programu,poprawienie petli while
    
    * ver.1.5
    
    * ver.1.6 drobne poprawki
    
    * 1
    
    * 1.1
    
    * sdaf
    
    * ver.1.6 drobne poprawki
    
    * parsowanie miast
    
    * 1.2 pobranie danych , porownianie , wynik
    
    * koniec 11.09
    
    * koniec 11.09
    
    * 1.2 pobranie danych , porownianie , wynik , zmiana typu String na double
    
    * 1.2 pobranie danych , porownianie , wynik , zmiana typu String na double, dodanie przeliczania km , brak bazy stacji !
    
    * 1.2 pobranie danych , porownianie , wynik , zmiana typu String na double, dodanie przeliczania km , brak bazy stacji !pomysl na jd2m-13
    
    * finish jd2m27 1109
    
    * Gotowe menu .
    
    * Gotowe obliczanie w odniesieniu o kordy
    
    * JD2M-14
    userchooseradius ,math  - not complete
    
    * JD2M-14
    userchooseradius ,math  - not complete
    
    * aktualny stan
    
    * aktualny stan
    
    * Menu
    
    * Menu z poprawkami
    
    * pare poprawek jeszcze nie skonczone
    
    * zmiana stringa na double w współrzędnych
    
    * pare poprawek jeszcze nie skonczone
    
    * Jd2m-25 poprawki
    
    * Jd2m-25 poprawki
    
    * Jd2m-12 poprawki prace trwaja
    
    * pare poprawek jeszcze nie skonczone
    
    * ogien !
    
    * ogien !
    
    * Jd2m-12 poprawki prace trwaja
    
    * Jd2m-13 poprawnienie klasy obliczajacej odleglosc , poprawienie geolokacji uzytkownika , powoli klejenie wszystkiego do kupy
    
    *  powoli klejenie wszystkiego do kupy
    
    *  powoli klejenie wszystkiego do kupy
    
    *  powoli klejenie wszystkiego do kupy
    
    *  powoli klejenie wszystkiego do kupy
    
    * dzialajaca klasa obliczania , szukanie najbliszej stacji dziala , wyliczanie odleglosci dziala . sen jest dla slabych ogien !
    
    * statystyki
    
    * WSZYSTKO SKONCZONE !!!!! FUCK YEAH !!!
    
    * Poprawienie pętli, poprawienie wprowadzania współrzędnych, poprawa struktury menu
    
    * All done ,hot fixy zrobione .
    
    * All done ,hot fixy zrobione .
    
    * SORTOWANIE STATYSTYK
    
    * ogien !
    
    * hot fix
    
    * hot fix
    
    * hot fix
    
    * hot fix

---
+++ src/infoshare/kurs/mohito/rowery/City.java
@@ -0,0 +1,54 @@
+package infoshare.kurs.mohito.rowery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class City {
+
+
+    private String countryName;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private String name;
+
+    private List<Place> placeList;
+
+    protected City(Double latitude, Double longitude, String name, String country) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.name = name;
+        this.countryName = country;
+        this.placeList = new ArrayList<Place>();
+    }
+
+    @Override
+    public String toString() {
+        return name + "[" +
+                latitude +
+                "/" + longitude +
+                ']';
+    }
+
+    protected Double getLatitude() {
+        return latitude;
+    }
+
+    protected Double getLongitude() {
+        return longitude;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    protected List<Place> getPlaceList() {
+        return placeList;
+    }
+
+    protected String getCountryName() {
+        return countryName;
+    }
+}
