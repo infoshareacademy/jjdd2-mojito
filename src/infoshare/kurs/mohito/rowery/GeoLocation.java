52e783eee1bbc419d1e65dffafa73c7afeb01bce
mkoniuszy <m.koniuszy@vp.pl>
2017-09-18 11:15:21 +0200

    hot fix

--- src/infoshare/kurs/mohito/rowery/GeoLocation.java
+++ src/infoshare/kurs/mohito/rowery/GeoLocation.java
@@ -20,20 +20,16 @@ public class GeoLocation {
 
         boolean done = false;
         while (!done) {
-
-            //sprawdzenie, czy wpisana współrzędna jest liczbą
             try {
-
                 System.out.println("podaj pierwsza wspolrzedna ");
                 String pierwszastring = new UserInputReader().readlineString();
-                pierwszastring = pierwszastring.replace(",", ".");// zamienia kropki na przecinki
-                double pierwsza = Double.parseDouble(pierwszastring);//parsowanie z string na double
-                pierwsza = Math.abs(pierwsza);//zwraca wartość bezwzględną, w przypadku gdy ktoś wpisze z minusem
+                pierwszastring = pierwszastring.replace(",", ".");
+                double pierwsza = Double.parseDouble(pierwszastring);
+                pierwsza = Math.abs(pierwsza);
                 wspolrzedne[0] = pierwsza;
                 this.latitiudeUser = wspolrzedne[0];
                 this.longitudeUser = wspolrzedne[1];
                 done = true;
-
             } catch (NumberFormatException e) {
                 System.out.println("niepoprawny format, podaj pierwsza wspolrzedna ponownie");
 
@@ -42,12 +38,11 @@ public class GeoLocation {
         done = false;
 
         while (!done) {
-
             try {
 
                 System.out.println("podaj druga wspolrzedna ");
                 String drugastring = new UserInputReader().readlineString();
-                drugastring = drugastring.replace(",", ".");// zamienia kropki na przecinki
+                drugastring = drugastring.replace(",", ".");
                 double druga = Double.parseDouble(drugastring);//parsowanie z string na double
                 druga = Math.abs(druga);
                 wspolrzedne[1] = druga;
