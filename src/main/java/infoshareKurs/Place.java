31377032c2ec052063fedb70bd8375dbc4ec9bb6
mkoniuszy <m.koniuszy@vp.pl>
2017-09-20 16:43:07 +0200

    JD2M-30  , zmiana archtypuMaven

---
+++ src/main/java/infoshareKurs/Place.java
@@ -0,0 +1,31 @@
+package infoshareKurs;
+
+public class Place {
+
+    private String name;
+
+    private GeoLocation geoLocation;
+
+    public Place(String name, double latitiudePlace, double longitudePlace) {
+
+        this.name = name;
+        this.latitiudePlace = latitiudePlace;
+        this.longitudePlace = longitudePlace;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    protected double getLatitiudePlace() {
+        return latitiudePlace;
+    }
+
+    protected double getLongitudePlace() {
+        return longitudePlace;
+    }
+
+    private double latitiudePlace;
+
+    private double longitudePlace;
+}
