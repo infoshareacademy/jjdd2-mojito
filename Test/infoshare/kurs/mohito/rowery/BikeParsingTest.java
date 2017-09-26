70dda521fe59bc97f3760aec64b9646c3fcdb590
TZmitrowicz <tzmitrowicz@wp.pl>
2017-09-26 15:26:28 +0200

    test

--- Test/infoshare/kurs/mohito/rowery/BikeParsingTest.java
+++ Test/infoshare/kurs/mohito/rowery/BikeParsingTest.java
@@ -8,6 +8,9 @@ public class BikeParsingTest {
 
     @Test
     public void test(){
+        BikeParsing bikeParsing = new BikeParsing("data/nextbike-test.xml");
+        bikeParsing.parseData();
+        assertEquals (7, bikeParsing.getCityList().size());
 
     }
 
