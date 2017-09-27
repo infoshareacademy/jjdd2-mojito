1515e1e464859a538d862788f94db6d3a493ccf7
mkoniuszy <m.koniuszy@vp.pl>
2017-09-27 22:06:50 +0200

    JD2M - 37 ,opcja menu apk.web , 2,3 work .

--- src/main/java/infoshareKurs/web/CityStationsServlet.java
+++ src/main/java/infoshareKurs/web/CityStationsServlet.java
@@ -18,64 +18,62 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/city_stations")
-public class CityStationsServlet extends HttpServlet{
+public class CityStationsServlet extends HttpServlet {
 
-        @Override
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-            resp.setContentType("text/html;charset=UTF-8");
-            PrintWriter writer = resp.getWriter();
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
 
-            writer.println("<!DOCTYPE html>");
-            writer.println("<html>");
-            writer.println("<body>");
-            writer.println("<h1>\"Wpisz nazwę interesującego Cię Miasta. </h1>");
-            writer.println("</form>");
-            writer.println("<form action=\"country_stations\" method=\"post\">");
-            writer.println("<input type=\"text\" name=\"userCity\"/>");
-            writer.println("<button type=\"submit\" />Send</button>");
-            writer.println("</form>");
-            writer.println("</body>");
-            writer.println("</html>");
+        writer.println("<!DOCTYPE html>");
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("<h1>\"Wpisz nazwę interesującego Cię Miasta. </h1>");
+        writer.println("</form>");
+        writer.println("<form action=\"city_stations\" method=\"post\">");
+        writer.println("<input type=\"text\" name=\"userCity\"/>");
+        writer.println("<button type=\"submit\" />Send</button>");
+        writer.println("</form>");
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+
+        final Logger logger = LogManager.getLogger(CountryStations.class);
+        final BikeParsing bikeParsing = new BikeParsing(System.getProperty("java.io.tmpdir") + "/plik");
+        try {
+            bikeParsing.parseData();
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            logger.error("błąd parsowania pliku xml");
+            e.printStackTrace();
         }
 
-        @Override
-        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-            resp.setContentType("text/html;charset=UTF-8");
-            PrintWriter writer = resp.getWriter();
-
-            final Logger logger = LogManager.getLogger(CountryStations.class);
-            final BikeParsing bikeParsing = new BikeParsing(System.getProperty("java.io.tmpdir") + "/plik");
-            try {
-                bikeParsing.parseData();
-            } catch (ParserConfigurationException | SAXException | IOException e) {
-                logger.error("błąd parsowania pliku xml");
-                e.printStackTrace();
-            }
-
-            boolean done = false;
-            while (!done) {
-                String inputdata = req.getParameter("userCity");
-                int i = 0;
-                writer.println("Stacje rowerowe znajdujące sie w  " + inputdata);
-                for (City city : bikeParsing.getCityList()) {
-                    if (city.getName().equals(inputdata)) {
-                        i++;
-                        for (Place place : city.getPlaceList()) {
-                            writer.println(place.getName());
-                            writer.println("<br>");
-                            logger.debug("wypisanie stacji rowerowych znajdujacych sie danym kraju");
-
-                        }
+        boolean done = false;
+        while (!done) {
+            String inputdata = req.getParameter("userCity");
+            int i = 0;
+            writer.println("Stacje rowerowe znajdujące sie w  " + inputdata);
+            for (City city : bikeParsing.getCityList()) {
+                if (city.getName().equals(inputdata)) {
+                    i++;
+                    for (Place place : city.getPlaceList()) {
+                        writer.println(place.getName());
+                        writer.println("<br>");
+                        logger.debug("wypisanie stacji rowerowych znajdujacych sie danym kraju");
                     }
                     done = true;
                 }
-                if (i == 0) {
-                    writer.println("Nie znaleziono miasta w bazie, wprowadź nazwę ponownie.");
-                    logger.info("nie znaleziono miasta w bazie danych ");
-                    done = true;
-                }
+
+            }
+            if (i == 0) {
+                writer.println("Nie znaleziono miasta w bazie, wprowadź nazwę miasta ponownie.");
             }
         }
     }
+}
