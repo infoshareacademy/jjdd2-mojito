eec4a7873e60980c29fcdc5afd593ad50e658e3e
mkoniuszy <m.koniuszy@vp.pl>
2017-09-27 22:29:35 +0200

    JD2M - 37 ,opcja menu apk.web , 2,3 work .

--- src/main/java/infoshareKurs/web/NearestStationServlet.java
+++ src/main/java/infoshareKurs/web/NearestStationServlet.java
@@ -13,7 +13,31 @@ public class NearestStationServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter pw = resp.getWriter();
-        pw.println("Szukamy najblizszej stacji");
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+
+        writer.println("<!DOCTYPE html>");
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("<h1>\"Podaj swoje położenie geograficzną\"  </h1>");
+        writer.println("<form action=\"nearest_station\" method=\"post\">");
+        writer.println("<h2>\"Podaj szerokość geograficzną \n wzór XXXX.XXXX\"  </h2>");
+        writer.println("<form action=\"nearest_station\" method=\"post\">");
+        writer.println("<input type=\"text\" name=\"latitiudeUser\"/>");
+        writer.println("<h2>\"Podaj szerokość geograficzną \n wzór XXXX.XXXX\"  </h2>");
+        writer.println("</form>");
+        writer.println("<form action=\"nearest_station\" method=\"post\">");
+        writer.println("<input type=\"text\" name=\"longitudeUser\"/>");
+        writer.println("<button type=\"submit\" />Send</button>");
+        writer.println("</form>");
+        writer.println("</body>");
+        writer.println("</html>");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
     }
 }
