ebfb3bb1e424729327375fa221265995d7c5c97d
mkoniuszy <m.koniuszy@vp.pl>
2017-10-25 23:12:34 +0200

    JD2M-86 finplace jsp

--- src/main/java/infoshareKurs/web/FindPlaceServlet.java
+++ src/main/java/infoshareKurs/web/FindPlaceServlet.java
@@ -26,84 +26,8 @@ public class FindPlaceServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        resp.setContentType("text/html;charset=UTF-8");
-
-        PrintWriter writer = resp.getWriter();
-
-        writer.println("<!DOCTYPE html>" +
-                "<html>" +
-                "<head>" +
-                "<link rel=\"stylesheet\" " +
-                "href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css\"" +
-                " integrity=\"sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M\"" +
-                " crossorigin=\"anonymous\">\n" +
-                "<script src=\"https://code.jquery.com/jquery-3.1.1.slim.min.js\" " +
-                "integrity=\"sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n\"" +
-                " crossorigin=\"anonymous\"></script>\n" +
-                "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js\"" +
-                " integrity=\"sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb\" " +
-                "crossorigin=\"anonymous\"></script>\n" +
-                "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js\"" +
-                " integrity=\"sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn\"" +
-                " crossorigin=\"anonymous\"></script>" +
-                "</head>" +
-                "<body class=\"bg-dark\">" +
-                "<nav class=\"navbar navbar-dark bg-dark\" style=\" position: fixed;\n" +
-                "    top: 0;\n" +
-                "    width: 100%;\n" +
-                "    border-bottom: 1px solid #666;" +
-                "    z-index: 1;\">\n" +
-                "  <span class=\"navbar-brand\"><b>Mohito Bike Project</b></span>\n" +
-                "  <button class=\"navbar-toggler navbar-toggler-right\" type=\"button\"" +
-                " data-toggle=\"collapse\" data-target=\"#navbarText\" aria-controls=\"navbarText\"" +
-                " aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
-                "    <span class=\"navbar-toggler-icon\"></span>\n" +
-                "  </button>\n" +
-                "  <div class=\"collapse navbar-collapse\" id=\"navbarText\">\n" +
-                "    <ul class=\"navbar-nav mr-auto\">\n" +
-                "      <li class=\"nav-item\">\n" +
-                "        <a class=\"nav-link\" href=\"nearestStation\">Najblizsza stacja</a>\n" +
-                "      </li>\n" +
-                "      <li class=\"nav-item\">\n" +
-                "        <a class=\"nav-link\" href=\"country_stations\">stacje w kraju</a>\n" +
-                "      </li>\n" +
-                "      <li class=\"nav-item\">\n" +
-                "        <a class=\"nav-link\" href=\"city_stations\">stacje w miescie</a>\n" +
-                "      </li>\n" +
-                "      <li class=\"nav-item\">\n" +
-                "        <a class=\"nav-link\" href=\"FindPlaceServlet\">wyszukanie stacji w promieniu</a>\n" +
-                "      </li>\n" +
-                "      <li class=\"nav-item\">\n" +
-                "        <a class=\"nav-link\" href=\"countryStat\">statystyki panst</a>\n" +
-                "      </li>\n" +
-                "      <li class=\"nav-item\">\n" +
-                "        <a class=\"nav-link\" href=\"cityStat\">statystyki miast</a>\n" +
-                "      </li>\n" +
-                "       <li class=\"nav-item\">\n" +
-                "        <a class=\"nav-link\" href=\"logout\">wylogowanie</a>\n" +
-                "     </li>\n" +
-                "    </ul>\n" +
-                "  </div>\n" +
-                "</nav>" +
-                "<div style=\"margin-top:15%;\">\n" +
-                "      \n" +
-                "      <div class=\"text-center\">" +
-                "<h2 class=\"text-white\">Wpisz odległosć wyszukiwania stacji. </h2>" +
-                "</form>" +
-                "<form action=\"FindPlaceServlet\" method=\"post\">" +
-                "<input type=\"number\" name=\"choosenRadius\"/>" +
-                "<h2 class=\"text-white\">Podaj szerokość geograficzną \n wzór XXXX.XXXX\"</h2>" +
-                "<form action=\"nearestStation\" method=\"post\">" +
-                "<input type=\"text\"name=\"latitiudeUser\"/>" +
-                "<h2 class=\"text-white\">Podaj długość geograficzną \n wzór XXXX.XXXX\"  </h2>" +
-                "<input type=\"text\"name=\"longitudeUser\"/>" +
-                "<button class=\"btn btn-secondary btn-lg\" type=\"submit\" />Znajdz</button>" +
-                "</form>" +
-                "</div>" +
-                "</div>" +
-                "</body>" +
-                "</html>");
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/findplaceGET.jsp");
+        requestDispatcher.forward(req, resp);
     }
 
     @Override
